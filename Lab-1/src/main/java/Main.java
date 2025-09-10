8655d7af7aeb2a2d1484bc10e515df41c2d0fd80
kytaec <vladislavtokovenko4@gmail.com>
2025-09-10 11:25:15 +0300

    changes

--- Lab-1/src/main/java/Main.java
+++ Lab-1/src/main/java/Main.java
@@ -1,12 +1,14 @@
 import java.util.*;
 
+//  5. Знайти слова, які складаються тільки з різних символів. На виході – масив String.
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Введіть рядок зі словами:");
         String input = sc.nextLine();
 
-        String[] words = input.split(" ");
+        String[] words = input.split("\\s+");
 
         List<String> result = new ArrayList<>();
 
